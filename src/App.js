6d50fd5669b5125cd7578d6f57dcc05bbde2ae53
SumitGarg007 <sumitexpinator@gmail.com>
2021-06-07 13:27:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import LandingPage from "./pages/Landing_page /landingPage";
+import UserDashboard from "./pages/user_dashboard_apge/user_dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+      {/* <UserDashboard /> */}
+    </div>
+  );
+}
+
+export default App;
